d9840c061f1ce6e2be96c5b2f3c633be7a0e8e7d
Hristo <hristo_ixa@abv.bg>
2020-07-02 15:29:17 +0300

    test

---
+++ src/test/java/bg/cholakov/pma/PmaApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.cholakov.pma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
